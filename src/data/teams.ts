df387c13163a826d8c8e04ebe3a38fe41cd0b8b0
Greg Domanin <gregorydomanin@gmail.com>
2021-05-10 00:48:05 -0500

    Fix types in teams.ts. Export typeguard functions

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,7 +1,7 @@
 import type { ITeam } from '../types';
 import { apiCall } from '../utils/networking';
 
-const isITeam = (arg: any): arg is ITeam => {
+export const isITeam = (arg: any): arg is ITeam => {
   /**
    * iconUrl: string;
       name: string;
@@ -16,7 +16,7 @@ const isITeam = (arg: any): arg is ITeam => {
   );
 };
 
-function assertIsTypedArray<T>(
+export function assertIsTypedArray<T>(
   arg: any,
   check: (val: any) => val is T,
 ): asserts arg is T[] {
